7a4d6b641fa37b6a1fe45c2a5b2210300b25c19d
Lcdemi <Lcd4910@rit.edu>
2025-03-05 15:19:22 -0500

    Removed Linux Compatability for now

--- Host.cpp
+++ Host.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-
-#ifdef _WIN32
 #include <winsock2.h> // Core Sockets (Windows)
-#else
-#include <sys/socket.h> // Core Sockets (Linux)
-#endif
+#include <windows.h>
 
 #define DEFAULT_CLIENT_IP "127.0.0.1"
 #define DEFAULT_SERVER_IP "127.0.0.1"
@@ -12,7 +8,6 @@
 #define BUFFER_SIZE 1024
 
 struct SocketInfo {
-    SOCKET linuxClientSocket;
     SOCKET windowsClientSocket;
     sockaddr_in serverAddress;
 };
@@ -35,21 +30,7 @@ SocketInfo createSocketWindows() {
     serverAddress.sin_family = AF_INET; // IPv4
 
     std::cout << "Socket created!" << std::endl;
-    return {INVALID_SOCKET, windowsClientSocket, serverAddress};
-}
-
-SocketInfo createSocketLinux() {
-    SOCKET linuxClientSocket = socket(AF_INET, SOCK_STREAM, 0); // Creates Socket
-    if (linuxClientSocket == INVALID_SOCKET) {
-        perror("Socket creation failed!");
-        exit(1);
-    }
-
-    sockaddr_in serverAddress; // Stores Address of Socket
-    serverAddress.sin_family = AF_INET; // IPv4
-
-    std::cout << "Socket created!" << std::endl;
-    return {linuxClientSocket, INVALID_SOCKET, serverAddress};
+    return {windowsClientSocket, serverAddress};
 }
 
 void connectClientWindows(SOCKET clientSocket, sockaddr_in serverAddress) {
@@ -57,13 +38,6 @@ void connectClientWindows(SOCKET clientSocket, sockaddr_in serverAddress) {
         std::cerr << "Socket connection failed: " << WSAGetLastError() << std::endl;
         closesocket(clientSocket); // Closes Socket
         WSACleanup(); // Terminates Winsock
-        exit(1);
-    }
-}
-
-void connectClientLinux(int clientSocket, sockaddr_in serverAddress) {
-    if (connect(static_cast<SOCKET>(clientSocket), (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) { // Connects to Open Server Socket
-        perror("Socket connection failed!");
         exit(1);
     }
 }
@@ -80,19 +54,11 @@ int main(int argc, char* argv[]) {
     std::cout << "Connecting to Server IP: " << serverIP << " on Port: " << port << std::endl;
 
     SocketInfo socketInfo;
-    #ifdef _WIN32 // Determines the OS (Windows or Linux)
     std::cout << "Running on Windows" << std::endl;
     socketInfo = createSocketWindows();
     socketInfo.serverAddress.sin_port = htons(static_cast<u_short>(port)); // Set Server Port
     socketInfo.serverAddress.sin_addr.s_addr = inet_addr(serverIP.c_str()); // Set Server IP
     connectClientWindows(socketInfo.windowsClientSocket, socketInfo.serverAddress);
-    #else
-    std::cout << "Running on Linux" << std::endl;
-    socketInfo = createSocketLinux();
-    socketInfo.serverAddress.sin_port = htons(port); // Set Server Port
-    socketInfo.serverAddress.sin_addr.s_addr = inet_addr(serverIP.c_str()); // Set Server IP
-    connectClientLinux(socketInfo.linuxClientSocket, socketInfo.serverAddress);
-    #endif
 
     return 0;
 }
--- Payload.cpp
+++ Payload.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-
-#ifdef _WIN32
 #include <winsock2.h> // Core Sockets (Windows)
-#else
-#include <sys/socket.h> // Core Sockets (Linux)
-#endif
+#include <windows.h>
 
 #define DEFAULT_CLIENT_IP "127.0.0.1"
 #define DEFAULT_SERVER_IP "127.0.0.1"
@@ -12,7 +8,6 @@
 #define BUFFER_SIZE 1024
 
 struct SocketInfo {
-    SOCKET linuxClientSocket;
     SOCKET windowsClientSocket;
     sockaddr_in serverAddress;
 };
@@ -35,21 +30,7 @@ SocketInfo createSocketWindows() {
     serverAddress.sin_family = AF_INET; // IPv4
 
     std::cout << "Socket created!" << std::endl;
-    return {INVALID_SOCKET, windowsServerSocket, serverAddress};
-}
-
-SocketInfo createSocketLinux() {
-    SOCKET linuxServerSocket = socket(AF_INET, SOCK_STREAM, 0); // Creates Socket
-    if (linuxServerSocket == INVALID_SOCKET) {
-        perror("Socket creation failed!");
-        exit(1);
-    }
-
-    sockaddr_in serverAddress; // Stores Address of Socket
-    serverAddress.sin_family = AF_INET; // IPv4
-
-    std::cout << "Socket created!" << std::endl;
-    return {linuxServerSocket, INVALID_SOCKET, serverAddress};
+    return {windowsServerSocket, serverAddress};
 }
 
 void connectSocketWindows(SOCKET serverSocket, sockaddr_in serverAddress) {
@@ -82,33 +63,6 @@ void connectSocketWindows(SOCKET serverSocket, sockaddr_in serverAddress) {
     std::cout << "Windows socket accepted connection!" << std::endl;
 }
 
-void connectSocketLinux(SOCKET serverSocket, sockaddr_in serverAddress) {
-    if (bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) { // Binds Socket to Address
-        perror("Socket binding failed!");
-        closesocket(serverSocket); // Closes Socket
-        exit(1);
-    }
-
-    std::cout << "Linux socket bound successfully!" << std::endl;
-
-    if (listen(serverSocket, SOMAXCONN) == -1) { // Listens for Connections
-        perror("Socket listening failed!");
-        closesocket(serverSocket); // Closes Socket
-        exit(1);
-    }
-
-    std::cout << "Linux socket is now listening!" << std::endl;
-
-    SOCKET clientSocket = accept(serverSocket, NULL, NULL); // Accepts Connection
-    if (clientSocket == INVALID_SOCKET) {
-        perror("Socket accepting failed!");
-        closesocket(serverSocket); // Closes Socket
-        exit(1);
-    }
-
-    std::cout << "Linux socket accepted connection!" << std::endl;
-}
-
 int main(int argc, char* argv[]) {
     std::string clientIP = DEFAULT_CLIENT_IP;
     int port = DEFAULT_PORT;
@@ -121,19 +75,10 @@ int main(int argc, char* argv[]) {
     std::cout << "Port: " << port << std::endl;
 
     SocketInfo socketInfo;
-    #ifdef _WIN32 // Determines the OS (Windows or Linux)
     std::cout << "Running on Windows" << std::endl;
     socketInfo = createSocketWindows();
     socketInfo.serverAddress.sin_port = htons(static_cast<u_short>(port)); // Set Port
     socketInfo.serverAddress.sin_addr.s_addr = inet_addr(clientIP.c_str()); // Set Server IP
     connectSocketWindows(static_cast<SOCKET>(socketInfo.windowsClientSocket), socketInfo.serverAddress);
-    #else
-    std::cout << "Running on Linux" << std::endl;
-    socketInfo = createSocketLinux();
-    socketInfo.serverAddress.sin_port = htons(port); // Set Port
-    socketInfo.serverAddress.sin_addr.s_addr = inet_addr(serverIP.c_str()); // Set Server IP
-    connectClientLinux(socketInfo.linuxClientSocket, socketInfo.serverAddress);
-    #endif
-
     return 0;
 }
